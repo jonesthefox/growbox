800e176a8fe3a7e866f78b6b652e8c7f8ddedc92
fox <jonesthefox@icloud.com>
2022-06-07 19:40:45 +0200

    minor changes

--- frontend/tmpl/default/boxes/main-camera.php
+++ frontend/tmpl/default/boxes/main-camera.php
@@ -2,6 +2,6 @@
     <h6><i class="fa fa-image w3-margin-right w3-padding-small"></i><?=_PHOTO_CAMERA;?></h6>
         <a href="/projects/<?=Cfg::read('plant','project');?>/video.mp4">
             <!--suppress HtmlUnknownTarget -->
-            <img class="w3-image w3-round w3-border w3-border-black w3-margin-top w3-margin-bottom" src="/image/" width="640" height="480" style="background-image: url('<?=Bootstrap::TEMPLATEDIRHTML;?>/images/spinner-1280x960.gif'); background-size: 100%; background-repeat: no-repeat;" alt="<?=_IMG_ALT;?>">
+            <img class="w3-image w3-round w3-border w3-border-black w3-margin-bottom" src="/image/" width="640" height="480" style="background-image: url('<?=Bootstrap::TEMPLATEDIRHTML;?>/images/spinner-1280x960.gif'); background-size: 100%; background-repeat: no-repeat;" alt="<?=_IMG_ALT;?>">
         </a>
 </div>
--- frontend/tmpl/default/boxes/main-projectbrief.php
+++ frontend/tmpl/default/boxes/main-projectbrief.php
@@ -1,26 +1,12 @@
 <div class="w3-container w3-card w3-white w3-margin-bottom w3-round-large">
     <h6><i class="fa fa-folder w3-margin-right w3-padding-small"></i><?=_PLANT_ACTUALPROJECT;?></h6>
     <div class="w3-container w3-card w3-white w3-margin-bottom w3-round-large">
-        <table class="w3-table w3-striped w3-margin-bottom w3-tiny">
+        <table class="w3-table w3-tiny">
             <tr>
-                <td><i class="fa fa-seedling w3-margin-right w3-padding-small"></i></td>
-                <td><b><?=_PLANT_SPECIES;?></b></td>
-                <td class="w3-right"><?=Cfg::read('plant','species');?></td>
-            </tr>
-            <tr>
-                <td><i class="fa fa-calendar w3-margin-right w3-padding-small"></i></td>
-                <td><b><?=_PROJECT_ACTIVE;?></b></td>
-                <td class="w3-right"><?=Project::daysrunning(Cfg::read('plant','startdate'), date("Y-m-d"));?> <?=_PROJECT_DAYSRUNNING;?></td>
-            </tr>
-            <tr>
-                <td><i class="fa fa-leaf w3-margin-right w3-padding-small"></i></td>
-                <td><b><?=_PROJECT_GROWPHASE;?></b></td>
-                <td class="w3-right"><?=Project::growPhase()?></td>
-            </tr>
-            <tr>
-                <td><i class="fa fa-sun w3-padding-small"></i></td>
-                <td><b><?=_PLANT_DAYTIME;?></b></td>
-                <td class="w3-right"><?=Project::dayTime();?></td>
+                <td><b><?=_PLANT_SPECIES;?></b><br><?=Cfg::read('plant','species');?></td>
+                <td><b><?=_PROJECT_ACTIVE;?></b><br><?=Project::daysrunning(Cfg::read('plant','startdate'), date("Y-m-d"));?> <?=_PROJECT_DAYSRUNNING;?></td>
+                <td><b><?=_PROJECT_GROWPHASE;?></b><br><?=Project::growPhase()?></td>
+                <td><b><?=_PLANT_DAYTIME;?></b><br><?=Project::dayTime();?></td>
             </tr>
         </table>
     </div>
